8538b1daea9f243a182932bb01fdb9ae7994a250
DaryBena <darinabezyakina@gmail.com>
2023-02-20 01:37:05 +0200

    Add site and change location

--- src/index.js
+++ src/index.js
@@ -9,5 +9,15 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <Weather />
+    <small>
+      <a
+        href="https://github.com/DaryBena/vanilla-weather-app"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Open-source code,
+      </a>
+      by Dary Bena
+    </small>
   </StrictMode>
 );
